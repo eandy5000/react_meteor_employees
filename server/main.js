db54297dc7981bb5ce6a6d750a2ff31c84ec6add
Andy Eccleston <eandy5000@yahoo.com>
2016-12-14 09:10:08 -0600

    added server side and mongo employees collection

---
+++ server/main.js
@@ -0,0 +1,22 @@
+import { Meteor } from 'meteor/meteor'
+import { Employees } from '../imports/collections/employees'
+import { image, helpers } from 'faker'
+
+Meteor.startup(() => {
+
+  const numberRecords = Employees.find({}).count()
+  
+  console.log("Server side ", numberRecords)
+  
+  if (!numberRecords) {
+    _.times(5000, () => {
+      const {name, email, phone} = helpers.createCard()
+
+      Employees.insert({
+        name, email, phone,
+        avatar: image.avatar()
+      })
+
+    })
+  }
+})
