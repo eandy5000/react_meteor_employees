dc7109612151aefcc5ff70edb24c5a91908419dd
Andy Eccleston <eandy5000@yahoo.com>
2016-12-14 09:50:12 -0600

    ran meteor remove autopublish

--- client/main.js
+++ client/main.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import EmployeeList from './components/employee_list'
 
 const App = () => {
 
     return (
-        <div> I work
+        <div> 
+            <EmployeeList />
         </div>
     )
 }
